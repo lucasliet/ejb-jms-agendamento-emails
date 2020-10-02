4c02079dc232020c826e3ff980272301a8808264
lsoliveira <lsoliveira@magnasistemas.com.br>
2020-10-02 11:44:25 -0300

    'init'

---
+++ src/main/java/br/com/alura/servico/AgendamentoEmailServico.java
@@ -0,0 +1,14 @@
+package br.com.alura.servico;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class AgendamentoEmailServico {
+	
+	public List<String> listar(){
+		return List.of("joão@alura.com.br", "lucasliet@test.com");
+	}
+	
+}
