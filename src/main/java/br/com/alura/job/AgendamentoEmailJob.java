8bdeac874f910563115b76e72b092f95000c3d48
lsoliveira <lsoliveira@magnasistemas.com.br>
2020-10-02 15:09:30 -0300

    turn job to singleton

--- src/main/java/br/com/alura/job/AgendamentoEmailJob.java
+++ src/main/java/br/com/alura/job/AgendamentoEmailJob.java
@@ -3,24 +3,14 @@ package br.com.alura.job;
 import java.util.List;
 
 import javax.ejb.Schedule;
-import javax.ejb.Stateless;
+import javax.ejb.Singleton;
 import javax.inject.Inject;
 
 import br.com.alura.entidade.AgendamentoEmail;
 import br.com.alura.servico.AgendamentoEmailServico;
 
-@Stateless
+@Singleton
 public class AgendamentoEmailJob {
-	
-	private static AgendamentoEmailJob instance;
-	
-	private AgendamentoEmailJob() {}
-	
-	public synchronized static AgendamentoEmailJob getInstance() {
-		if (instance == null)
-			instance = new AgendamentoEmailJob();
-		return instance;
-	}
 	
 	@Inject
 	private AgendamentoEmailServico agendamentoEmailServico;
